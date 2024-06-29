c0844325b794d27bd2818580c3e851be8436ee71
Ella Petryshak <ellakom21@gmail.com>
2024-06-29 09:16:43 +0200

    commit de exersicis

---
+++ variables.js
@@ -0,0 +1,22 @@
+//userName es una variable que tiene dentro una string
+userName = "Ella";
+userSurname = 'Petryshak';
+const ESPACIO = " ";
+let userTitle = 'Student';//una string con backticks es un template literal
+console.log(userName)
+console.log("Ella");
+console.log(userSurname);
+console.log(userTitle);
+console.log(userSurname, userName, userTitle);
+console.log(userName + ' ' + userSurname + ' ' + userTitle);
+userName = "Rodrigo";
+console.log(`Que tal estas, ${userName} ${userSurname} ?`);
+
+userAge = 33; //Number
+userExperience = 10;
+userHasHair = true; //Boolean
+console.log(userName + userAge);
+console.log(userName + userExperience);
+
+console.log ("Edad: " + userAge, "anos de experiencia");
+//objects
