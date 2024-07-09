d9509dd1c865e8d6660b0983dde740ac2021d325
Ella Petryshak <ellakom21@gmail.com>
2024-07-09 16:18:41 +0200

    arrays objects add

--- arrays_objects.js
+++ arrays_objects.js
@@ -93,30 +93,42 @@ alumnes = [
 ];
 
 console.log(cotxes);
+console.log(cotxes.toString());
 
  //TODO: cread un array que contenga una lista de una de las propiedades de los objetos
 
- let llistaDeMarcas = [];
+ /*let llistaDeMarcas = [];
 for (let i = 0; i < cotxes.length; i++) {
   llistaDeMarcas.push(cotxes[i].marca);
 }
- console.log(llistaDeMarcas);
+ console.log(llistaDeMarcas);*/
+
+ llista = [cotxes[0].marca, cotxes[1].marca, cotxes[2].marca, cotxes[3].marca, cotxes[4].marca];
+ console.log(llista);
  
 
  //TODO: cread una variable que contenga la suma de una propiedad (que sea numerica)
- let sumaAnys = 0;
+ /*let sumaAnys = 0;
  for (let i = 0; i < cotxes.length; i++) {
    sumaAnys += cotxes[i].any;
  }
- console.log(sumaAnys)
-
+ console.log(sumaAnys)*/
+ let sumaAnys = cotxes[0].any + cotxes[1].any + cotxes[2].any + cotxes[3].any + cotxes[4].any
+console.log("Suma anys: " + sumaAnys);
  //TODO: monstrar por consola todo lo anterior
-
+ 
  //TODO: monstrar por consola  una frase que resuma las propiedades de cada objeto
- for (let i = 0; i < cotxes.length; i++) {
+ /*for (let i = 0; i < cotxes.length; i++) {
   const coche = cotxes[i];
-  console.log(`El Coche ${i + 1}: Marca ${coche.marca} el model ${coche.model} del any ${coche.any} de color ${coche.color}`);
-}
+  console.log(`El Cotxe ${i + 1}: Marca ${coche.marca} el model ${coche.model} del any ${coche.any} de color ${coche.color}`);
+}*/
+
+console.log(`El cotxe ${cotxes[0].marca} el model ${cotxes[0].model} del any ${cotxes[0].any} de color ${cotxes[0].color}`);
+console.log(`El cotxe ${cotxes[1].marca} el model ${cotxes[1].model} del any ${cotxes[1].any} de color ${cotxes[1].color}`);
+console.log(`El cotxe ${cotxes[2].marca} el model ${cotxes[2].model} del any ${cotxes[2].any} de color ${cotxes[2].color}`);
+console.log(`El cotxe ${cotxes[3].marca} el model ${cotxes[3].model} del any ${cotxes[3].any} de color ${cotxes[3].color}`);
+console.log(`El cotxe ${cotxes[4].marca} el model ${cotxes[4].model} del any ${cotxes[4].any} de color ${cotxes[4].color}`);
+
 
  //TODO: buscad una manera de mostrar todos los objetos por consola en forma de tabla
  console.table(cotxes);
