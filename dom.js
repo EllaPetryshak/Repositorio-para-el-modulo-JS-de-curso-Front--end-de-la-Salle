b839607993a0305684e5c41652983e64b7e0c5f0
Ella Petryshak <ellakom21@gmail.com>
2024-07-27 13:20:27 +0200

    add listers

--- dom.js
+++ dom.js
@@ -1,3 +1,4 @@
+
 console.log("Exercicis DOM");
 
 // tot el document HTML
@@ -11,11 +12,6 @@ console.log(document.URL);
 console.log(document.querySelector("h1").style)
 console.log(document.querySelector("h1").innerHTML)
 console.log(document.querySelector("h1").innerText)
-
-// accés a les propietats renderitzades d'un element (NO ÉS DOM!! ⚠)
-const titol = document.querySelector("h1")
-console.log(window.getComputedStyle(titol).getPropertyValue("color"))
-console.log(window.getComputedStyle(titol).getPropertyValue("font-family"))
 
 // innerHTMl vs innerText vs textContent
 console.log(document.querySelector("header").innerHTML)
@@ -31,7 +27,7 @@ console.log(document.querySelector("style").textContent)
 
 setTimeout(() => {
     document.querySelector("style").textContent = `
-body {
+        body {
             background-color: rgb(250, 249, 248);
             font-family: 'Courier New', Courier, monospace;
             margin: 0;
@@ -56,8 +52,10 @@ body {
             border-bottom: 1px solid #ddd;
         }
         img {
-            max-width: 100%;
+            max-height: 200px;
             height: auto;
+            float: left;
+            margin: 5px 15px 0 0;
         }
         form {
             max-width: 400px;
@@ -99,14 +97,6 @@ function canviarCSS() {
     document.querySelector("link[rel='stylesheet']").href = "../styles/dom_styles.css"
 }
 
-// selecciona més d'un element amb un selector
-const images = document.querySelectorAll("img")
-console.log(images);
-
-for (const img of images) {
-    img.src = "https://cataas.com/cat?width=600&height=400"
-}
-
 // afegir contingut a una secció
 section2 = document.querySelector("#section2") 
 section2OriginalContent = document.querySelector("#section2").innerHTML
@@ -114,6 +104,20 @@ section2OriginalContent = document.querySelector("#section2").innerHTML
 section2.innerHTML += '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, itaque! Explicabo quibusdam harum mollitia dolorem asperiores quis. Cupiditate est, perferendis quasi cumque iste molestiae reprehenderit!</p>'
 
 section2.innerHTML += '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus amet sunt voluptates consequuntur ad perspiciatis labore error impedit aliquid architecto veniam earum exercitationem aliquam tenetur, <span class="custom">blanditiis voluptatibus explicabo quod numquam enim fugiat!</span> Tenetur doloremque assumenda cupiditate blanditiis saepe necessitatibus omnis!</p>'
+
+// selecciona més d'un element amb un selector
+const images = document.querySelectorAll("img")
+console.log(images);
+
+fetch(`https://api.thecatapi.com/v1/images/search?limit=${images.length}`)
+.then(res => res.json())
+.then(res => {
+    console.log(res)
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].src = res[i].url
+    }
+})
 
 console.log(section2OriginalContent);
 console.log(section2.innerHTML);
@@ -124,3 +128,8 @@ customTexts[0].style.fontWeight = "bold"
 customTexts[1].style.fontStyle = "italic"
 customTexts[2].style.color = "#b13cb9"
 customTexts[3].style.fontVariant = "small-caps"
+
+// accés a les propietats renderitzades d'un element (NO ÉS DOM!! ⚠)
+const titol = document.querySelector("h1")
+console.log(window.getComputedStyle(titol).color)
+console.log(window.getComputedStyle(titol).fontFamily)
