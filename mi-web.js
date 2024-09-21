9e9a90cba1edfbdf6f766b92b9e0d2e66d5b5224
Ella Petryshak <ellakom21@gmail.com>
2024-09-21 09:49:58 +0200

    added changes my web

---
+++ mi-web.js
@@ -0,0 +1,16 @@
+// Referències als botons i al cos del document
+const lightThemeBtn = document.getElementById('lightThemeBtn');
+const darkThemeBtn = document.getElementById('darkThemeBtn');
+const body = document.body;
+
+// Funció per activar el tema clar
+lightThemeBtn.addEventListener('click', () => {
+  body.classList.remove('dark-theme');
+  body.classList.add('light-theme');
+});
+
+// Funció per activar el tema fosc
+darkThemeBtn.addEventListener('click', () => {
+  body.classList.remove('light-theme');
+  body.classList.add('dark-theme');
+});
